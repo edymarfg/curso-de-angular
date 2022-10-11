a1775d8b2a8e5cd6e9a4fa9e7deda9eb6cb60677
unknown <you@example.com>
2022-10-10 22:05:07 -0300

    template, injeção de dependência

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, CursosModule],
   providers: [],
   bootstrap: [AppComponent],
 })
