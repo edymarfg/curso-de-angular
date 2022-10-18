6c98ad953edc314b22d734eb1cf4d6da78423228
unknown <you@example.com>
2022-10-17 22:19:08 -0300

    Event binding, Two-way data binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.scss'],
+  //styleUrls: ['./data-binding.component.scss'],
+  styles: [
+    `
+      .highlight {
+        background-color: yellow;
+        font-weight: bold;
+      }
+    `,
+  ],
 })
 export class DataBindingComponent implements OnInit {
   url: string = 'http://loiane.com';
   cursoAngular: boolean = true;
   urlImagem =
     'https://i.picsum.photos/id/491/200/300.jpg?hmac=1uGno3XFc0HqGY5bM9-mMu0M_wwx7oC0bC1hj_a9p04';
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
 
   getValor() {
     return 1;
@@ -17,6 +30,22 @@ export class DataBindingComponent implements OnInit {
 
   getCurtirCurso() {
     return true;
+  }
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() {}
