3cc9f443c959b3f97dae40a1268d9f73a99d7f61
unknown <you@example.com>
2022-10-25 17:07:00 -0300

    adicionado eslint

--- diretivas/src/app/teste/teste.component.ts
+++ diretivas/src/app/teste/teste.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-teste',
   templateUrl: './teste.component.html',
-  styleUrls: ['./teste.component.scss']
+  styleUrls: ['./teste.component.scss'],
 })
 export class TesteComponent implements OnInit {
+  minhaVariavel: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
