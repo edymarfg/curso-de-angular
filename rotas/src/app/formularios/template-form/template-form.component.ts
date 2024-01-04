209037f343a6c6c6f71efc5ab604169c69b4a124
Edymar Freitas Goulart <edymar-fg10@hotmail.com>
2024-01-03 23:17:07 -0300

    binding

--- rotas/src/app/formularios/template-form/template-form.component.ts
+++ rotas/src/app/formularios/template-form/template-form.component.ts
@@ -7,9 +7,13 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./template-form.component.scss'],
 })
 export class TemplateFormComponent implements OnInit {
+  usuario: any = { nome: 'Teste', email: 'teste@email.com' };
+
   onSubmit(form: NgForm) {
-    console.log(form);
+    console.log(form.value);
+    console.log(this.usuario);
   }
+  // pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
 
   constructor() {}
 
