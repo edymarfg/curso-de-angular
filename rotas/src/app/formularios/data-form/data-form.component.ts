19377c22b9cf89b0144ba8ffd22be52b6212ec03
Edymar Freitas Goulart <edymar-fg10@hotmail.com>
2024-01-18 23:13:34 -0300

    formulario de endereco

--- rotas/src/app/formularios/data-form/data-form.component.ts
+++ rotas/src/app/formularios/data-form/data-form.component.ts
@@ -26,11 +26,30 @@ export class DataFormComponent implements OnInit {
     // this.form = new FormGroup({
     //   nome: new FormControl(null),
     //   email: new FormControl(null),
+    //   endereco: new FormGroup({
+    //     cep: new FormControl(null),
+    //     numero: new FormControl(null),
+    //     complemento: new FormControl(null),
+    //     rua: new FormControl(null),
+    //     bairro: new FormControl(null),
+    //     cidade: new FormControl(null),
+    //     estado: new FormControl(null),
+    //   }),
     // });
 
     this.form = this.formBuilder.group({
       nome: [null, Validators.required],
       email: [null, [Validators.required, Validators.email]],
+
+      endereco: this.formBuilder.group({
+        cep: [null, Validators.required],
+        numero: [null, Validators.required],
+        complemento: [null],
+        rua: [null, Validators.required],
+        bairro: [null, Validators.required],
+        cidade: [null, Validators.required],
+        estado: [null, Validators.required],
+      }),
     });
 
     // Validators.pattern(
@@ -64,17 +83,30 @@ export class DataFormComponent implements OnInit {
     this.form.reset();
   }
 
-  verificaValidTouched(campo: string) {
+  logif(val: boolean, text: string): void {
+    if (val) {
+      console.log(text);
+    }
+  }
+
+  verificaValidTouched(campo: string): boolean {
+    this.logif(
+      campo === 'nome',
+      '' + (this.form.get(campo)?.invalid && this.form.get(campo)?.touched) ??
+        false
+    );
     return (
       (this.form.get(campo)?.invalid && this.form.get(campo)?.touched) ?? false
     );
   }
 
-  verificaEmailInvalido() {
+  verificaEmailInvalido(): boolean {
     let campoEmail = this.form.get('email');
-    if (campoEmail?.errors) {
-      return this.verificaValidTouched('email') || campoEmail.errors?.['email'];
-    }
+
+    return (
+      this.verificaValidTouched('email') ||
+      (campoEmail?.errors?.['email'] ?? false)
+    );
   }
 
   aplicaCssErro(campo: string) {
@@ -82,5 +114,52 @@ export class DataFormComponent implements OnInit {
       'was-validated': this.verificaValidTouched(campo),
       'needs-validation': this.verificaValidTouched(campo),
     };
+  }
+
+  consultaCEP() {
+    let cep = this.form.get('endereco.cep')?.value;
+    console.log(cep);
+    cep = cep.replace(/\D/g, '');
+    if (cep != '') {
+      var validacep = /^[0-9]{8}$/;
+
+      if (validacep.test(cep)) {
+        this.resetaDadosForm();
+
+        this.httpClient
+          .get(`https://viacep.com.br/ws/${cep}/json`)
+          // .pipe(map((dados: any) => dados.json()))
+          .subscribe((dados) => this.populaDadosForm(dados));
+      }
+    }
+  }
+
+  populaDadosForm(dados: any) {
+    this.form.patchValue({
+      endereco: {
+        // cep: dados.cep,
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf,
+      },
+    });
+
+    this.form.get('nome')?.setValue('Teste');
+
+    console.log(this.form);
+  }
+
+  resetaDadosForm() {
+    this.form.patchValue({
+      endereco: {
+        complemento: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado: null,
+      },
+    });
   }
 }
